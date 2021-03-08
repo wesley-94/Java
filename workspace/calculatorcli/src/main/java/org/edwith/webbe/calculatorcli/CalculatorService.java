d826411be772331de9d8afd7134e2db440716d6d
wesley-94 <rladhlthf123@naver.com>
2021-03-08 15:25:02 +0900

    ADD 2021-03-08 Files

---
+++ workspace/calculatorcli/src/main/java/org/edwith/webbe/calculatorcli/CalculatorService.java
@@ -0,0 +1,20 @@
+package org.edwith.webbe.calculatorcli;
+
+public class CalculatorService {
+	public int plus(int value1, int value2) {
+		return value1 + value2;
+	}
+	
+	public int minus(int value1, int value2) {
+		return value1 - value2;
+	}
+	
+	public int multiple(int value1, int value2) {
+		return value1 * value2;
+	}
+	
+	public int divide(int value1, int value2) throws ArithmeticException {
+		return value1 / value2;
+	}
+	
+}
